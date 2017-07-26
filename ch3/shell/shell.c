8842fa232f9850a2a225c41d4daf92f37a878c22
Kaijie Chen <ckj996@gmail.com>
2017-07-26 11:30:38 +0800

    update shell: modularize

--- ch3/shell/shell.c
+++ ch3/shell/shell.c
@@ -11,6 +11,78 @@
 #define MAX_ARGN	((MAX_LINE + 1) / 2)
 #define HISTORY	10
 
+static char *history[HISTORY];
+static size_t count, head;
+
+void add_history(const char *line)
+{
+	if (count < HISTORY)
+		count++;
+	free(history[head]);
+	history[head] = strdup(line);
+	head = (head + 1) % HISTORY;
+}
+
+void print_history(void)
+{
+	for (size_t i = count; i; i--)
+		printf("%lu %s\n", i, history[(head + HISTORY - i) % HISTORY]);
+}
+
+void clear_history(void)
+{
+	for (size_t i = 0; i < HISTORY; i++) {
+		free(history[i]);
+		history[i] = NULL;
+	}
+}
+
+int check_history(char *line)
+{
+	size_t index, id;
+
+	if (line[0] != '!')
+		return 0;
+
+	if (line[1] == '!') {
+		if (count == 0) {
+			fprintf(stderr, "No commands in history.\n");
+			return -1;
+		}
+		index = (head + HISTORY - 1) % HISTORY;
+	} else if (isdigit(line[1])) {
+		sscanf(&line[1], "%lu", &id);
+		if (id == 0 || id > count) {
+			fprintf(stderr, "No such command in history\n");
+			return -1;
+		}
+		index = (head + HISTORY - id) % HISTORY;
+	} else {
+		fprintf(stderr, "Invalid syntax for history");
+		return -1;
+	}
+
+	strcpy(line, history[index]);
+	printf("%s\n", line);
+	return 0;
+}
+
+int check_background(int argn, char **args)
+{
+	char *last_arg = args[argn - 1];
+	size_t len = strlen(last_arg);
+
+	if (last_arg[len - 1] == '&') {
+		if (len > 1) {
+			last_arg[len - 1] = '\0';
+		} else {
+			args[argn - 1] = NULL;
+		}
+		return 1;
+	}
+	return 0;
+}
+
 int parse_line(char *str, char **args, int lim)
 {
 	char *token;
@@ -29,113 +101,86 @@ int parse_line(char *str, char **args, int lim)
 	return n;
 }
 
-int main(void)
+int read_line(char *buf, size_t size)
 {
-	char *history[HISTORY];
-	char *line;
-	char buf[MAX_LINE + 1];
-	char *args[MAX_ARGN + 1];
-	char *str;
-	int head = 0, count = 0;
-	int bg;
-	int argn;
-	int tmp, index;
-	int wstatus, rv;
-	pid_t pid;
 	size_t len;
+	int tmp;
 
-	memset(history, 0, sizeof(history));
+	if (fgets(buf, size, stdin) == NULL)
+		return EOF;
 
-	for (;;) {
-		printf("osh> ");
-		fflush(stdout);
-
-		if (fgets(buf, MAX_LINE + 1, stdin) == NULL)
-			break;
-
-		len = strlen(buf);
+	if ((len = strlen(buf)) > 0) {
 		if (buf[len - 1] == '\n') {
 			buf[len - 1] = '\0';
 		} else {
 			while ((tmp = getchar()) != EOF && tmp != '\n')
 				;
 		}
+	}
 
+	return 0;
+}
+
+int strempty(const char *s)
+{
+	for (const char *p = s; *p; p++)
+		if (!isspace(*p))
+			return 0;
+
+	return 1;
+}
+
+int main(void)
+{
+	char line[MAX_LINE + 1];
+	char *args[MAX_ARGN + 1];
+	int bg;
+	int argn;
+	int wstatus;
+	pid_t pid;
+
+	for (;;) {
 		while ((pid = waitpid(-1, &wstatus, WNOHANG)) > 0) {
 			printf("[bg done] %d\n", pid);
 		}
 
-		if (buf[0] == '!') {
-			if (buf[1] == '!') {
-				if (count == 0) {
-					fprintf(stderr, "No commands in history.\n");
-					continue;
-				}
-				index = (head + HISTORY - 1) % HISTORY;
-			} else if (isdigit(buf[1])) {
-				sscanf(&buf[1], "%d", &tmp);
-				if (tmp == 0 || tmp > count) {
-					fprintf(stderr, "No such commands in history\n");
-					continue;
-				}
-				index = (head + HISTORY - tmp) % HISTORY;
-			} else {
-				fprintf(stderr, "Invalid syntax");
-				continue;
-			}
-			strcpy(buf, history[index]);
-			printf("%s\n", buf);
-		}
+		printf("osh> ");
+		fflush(stdout);
 
-		line = strdup(buf);
+		if (read_line(line, MAX_LINE + 1) == EOF)
+			break;
 
-		argn = parse_line(buf, args, MAX_ARGN);
-
-		if (argn == 0) {
+		if (strempty(line))
 			continue;
-		} else {
-			bg = 0;
-			args[argn] = NULL;
-			str = args[argn - 1];
-			tmp = strlen(str);
-			if (str[tmp - 1] == '&') {
-				bg = 1;
-				if (tmp > 1)
-					str[tmp - 1] = '\0';
-				else
-					args[--argn] = NULL;
-			}
-		}
+
+		if (check_history(line) == -1)
+			continue;
+
+		add_history(line);
+
+		if ((argn = parse_line(line, args, MAX_ARGN)) == 0)
+			continue;
+
+		args[argn] = NULL;
+
+		bg = check_background(argn, args);
 
 		if (strcmp(args[0], "exit") == 0) {
 			break;
 		} else if (strcmp(args[0], "history") == 0) {
-			index = (head + HISTORY - count) % HISTORY;
-			for (int i = count; i > 0; i--) {
-				printf("%d %s\n", i, history[index]);
-				index = (index + 1) % HISTORY;
-			}
-			free(line);
-			continue;
+			print_history();
 		} else if ((pid = fork()) == 0) {
 			if (execvp(args[0], args) == -1) {
 				fprintf(stderr, "%s\n", strerror(errno));
 				break;
 			}
 		} else {
-			if (bg) {
+			if (bg)
 				printf("[bg run] %d\n", pid);
-			} else {
+			else
 				waitpid(pid, &wstatus, WUNTRACED);
-				rv = WEXITSTATUS(wstatus);
-			}
 		}
-
-		if (count < HISTORY)
-			count++;
-		free(history[head]);
-		history[head] = line;
-		head = (head + 1) % HISTORY;
 	}
+	clear_history();
 	return 0;
 }
