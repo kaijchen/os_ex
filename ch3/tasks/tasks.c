e005ac508c4f220b8cc7973b4abf36321ded297a
Kaijie Chen <ckj996@gmail.com>
2017-08-01 23:35:04 +0800

    add dfs_tasks

--- ch3/tasks/tasks.c
+++ ch3/tasks/tasks.c
@@ -1,6 +1,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/list.h>
 #include <linux/sched.h>
 #include <linux/sched/signal.h>
 
@@ -20,10 +21,27 @@ int tasks_init(void)
 	return 0;
 }
 
+void dfs_tasks(struct task_struct *this_task)
+{
+	struct task_struct *task;
+	struct list_head *list;
+
+	printk(KERN_INFO "%s %d\n",
+			this_task->comm,
+			task_pid_nr(this_task));
+
+	list_for_each(list, &this_task->children) {
+		task = list_entry(list, struct task_struct, sibling);
+
+		dfs_tasks(task);
+	}
+}
+
 /* This function is called when the module is removed */
 void tasks_exit(void)
 {
 	printk(KERN_INFO "Removing Module\n");
+	dfs_tasks(&init_task);
 }
 
 /* Marcos for regestering module entry and exit points */
